43792ef137afb172b017358a32ebe3dcd2d313e3
Bret Runestad <bret.runestad@gmail.com>
2015-01-20 14:52:01 -0500

    completed step 4, passing tests for reversed conversion

--- currency.py
+++ currency.py
@@ -2,7 +2,11 @@ def convert(rates, value, from_string, to_string):
     """Takes a list of rates, and takes an amount of currency(value),
     and """
     tuple_list = [x for x in rates if x[0]==from_string and x[1]==to_string]
-    return tuple_list[0][2] * value
+    if tuple_list:
+        return round(tuple_list[0][2] * value, 2)
+    else:
+        tuple_list2 = [x for x in rates if x[1]==from_string and x[0]==to_string]
+        return round((1/tuple_list2[0][2]) * value, 2)
 
 
 
--- test_currency.py
+++ test_currency.py
@@ -7,17 +7,22 @@ def test_convert_value_check():
                    to_string = 'USD') == 5
 
 def test_convert_dollar_to_euro():
-    rates = [('USD', 'EUR', .74), ('USD', 'JPY', 1.2)]
+    rates = [('USD', 'EUR', .74), ('USD', 'JPY', 145.949)]
     assert convert(rates,
                    value = 1,
                    from_string = 'USD',
                    to_string = 'EUR') == 0.74
 
-def xtest_convert_different_values():
-    rates = [('USD', 'EUR', .74), ('USD', 'JPY', 1.2)]
+def test_convert_different_values():
+    rates = [('USD', 'EUR', .74), ('USD', 'JPY', 145.949)]
     assert convert(rates, value = 5,
                    from_string = 'USD', to_string ='EUR') == 3.7
     assert convert(rates, value = .2,
-                   from_string = 'USD', to_string = 'EUR') == 0.148
+                   from_string = 'USD', to_string = 'EUR') == 0.15
     assert convert(rates, value = 0,
                    from_string = 'USD', to_string = 'EUR') == 0
+
+def test_flipped_conversion():
+    rates = [('USD', 'EUR', .74), ('USD', 'JPY', 145.949)]
+    assert convert(rates, value = 1,
+                   from_string = 'EUR', to_string = 'USD') == 1.35
