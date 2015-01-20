afe3a87bb31703bbe731fa45c5659654e574357a
Bret Runestad <bret.runestad@gmail.com>
2015-01-20 14:30:49 -0500

    Added test to pass 3rd step

--- test_currency.py
+++ test_currency.py
@@ -8,7 +8,16 @@ def test_convert_value_check():
 
 def test_convert_dollar_to_euro():
     rates = [('USD', 'EUR', .74), ('USD', 'JPY', 1.2)]
-    assert convert(rates = [('USD', 'EUR', .74)],
+    assert convert(rates,
                    value = 1,
                    from_string = 'USD',
                    to_string = 'EUR') == 0.74
+
+def xtest_convert_different_values():
+    rates = [('USD', 'EUR', .74), ('USD', 'JPY', 1.2)]
+    assert convert(rates, value = 5,
+                   from_string = 'USD', to_string ='EUR') == 3.7
+    assert convert(rates, value = .2,
+                   from_string = 'USD', to_string = 'EUR') == 0.148
+    assert convert(rates, value = 0,
+                   from_string = 'USD', to_string = 'EUR') == 0
