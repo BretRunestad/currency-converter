647071f19e7e5b4de5ec6c9e0f1ea2b2d4f8789a
Bret Runestad <bret.runestad@gmail.com>
2015-01-20 14:17:11 -0500

    testing for and passing 2nd step

--- currency.py
+++ currency.py
@@ -1,8 +1,14 @@
 def convert(rates, value, from_string, to_string):
-    return value
+    """Takes a list of rates, and takes an amount of currency(value),
+    and """
+    tuple_list = [x for x in rates if x[0]==from_string and x[1]==to_string]
+    return tuple_list[0][2] * value
 
 
 
 
 if __name__ == "__main__":
     pass
+
+rates = [("USD", "EUR", 0.74)]
+print(convert(rates, 1, "USD", "EUR"))
--- test_currency.py
+++ test_currency.py
@@ -1,7 +1,14 @@
 from currency import*
 
 def test_convert_value_check():
-    assert convert(rates=.75,
+    assert convert(rates= [('USD', 'USD', 1)],
                    value = 5,
                    from_string = 'USD',
                    to_string = 'USD') == 5
+
+def test_convert_dollar_to_euro():
+    rates = [('USD', 'EUR', .74), ('USD', 'JPY', 1.2)]
+    assert convert(rates = [('USD', 'EUR', .74)],
+                   value = 1,
+                   from_string = 'USD',
+                   to_string = 'EUR') == 0.74
