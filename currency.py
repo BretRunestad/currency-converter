6e4863643a21e8269ec8b7aee830e49ad85c015b
Bret Runestad <bret.runestad@gmail.com>
2015-01-20 20:07:49 -0500

    pep8 formatting

--- currency.py
+++ currency.py
@@ -4,12 +4,12 @@ def convert(rates, value, from_string, to_string):
     applicable currency exchange rate.  It then multiplies the value by that
     rate, rounded to 2 decimal places."""
     tuple_list = (
-    [x for x in rates if x[0] == from_string and x[1] == to_string])
+        [x for x in rates if x[0] == from_string and x[1] == to_string])
     if tuple_list:
         return round(tuple_list[0][2] * value, 2)
     else:
         tuple_list2 = (
-        [x for x in rates if x[1] == from_string and x[0] == to_string])
+            [x for x in rates if x[1] == from_string and x[0] == to_string])
         return round((1/tuple_list2[0][2]) * value, 2)
 
 
@@ -20,11 +20,10 @@ def exception_check(rates, value, from_string, to_string):
         return convert(rates, value, from_string, to_string)
     except IndexError:
         print(
-        "Sorry, at least one of those currency codes is not in my database")
+            "Sorry, at least one of the currency codes is not in my database")
         return ("Error message")
 
 
 if __name__ == "__main__":
-
 
     rates = [("USD", "EUR", 0.74)]
--- test_currency.py
+++ test_currency.py
@@ -1,43 +1,62 @@
 from currency import*
 
+
 def test_convert_value_check():
-    assert convert(rates= [('USD', 'USD', 1)],
-                   value = 5,
-                   from_string = 'USD',
-                   to_string = 'USD') == 5
+    assert convert(rates=[('USD', 'USD', 1)],
+                   value=5,
+                   from_string='USD',
+                   to_string='USD') == 5
+
 
 def test_convert_dollar_to_euro():
     rates = [('USD', 'EUR', .74), ('USD', 'JPY', 145.949)]
     assert convert(rates,
-                   value = 1,
-                   from_string = 'USD',
-                   to_string = 'EUR') == 0.74
+                   value=1,
+                   from_string='USD',
+                   to_string='EUR') == 0.74
+
 
 def test_convert_different_values():
     rates = [('USD', 'EUR', .74), ('USD', 'JPY', 145.949)]
-    assert convert(rates, value = 5,
-                   from_string = 'USD', to_string ='EUR') == 3.7
-    assert convert(rates, value = .2,
-                   from_string = 'USD', to_string = 'EUR') == 0.15
-    assert convert(rates, value = 0,
-                   from_string = 'USD', to_string = 'EUR') == 0
+    assert convert(rates, value=5,
+                   from_string='USD', to_string='EUR') == 3.7
+    assert convert(rates, value=.2,
+                   from_string='USD', to_string='EUR') == 0.15
+    assert convert(rates, value=0,
+                   from_string='USD', to_string='EUR') == 0
+
 
 def test_flipped_conversion():
     rates = [('USD', 'EUR', .74), ('USD', 'JPY', 145.949)]
-    assert convert(rates, value = 1,
-                   from_string = 'EUR', to_string = 'USD') == 1.35
+    assert convert(rates, value=1,
+                   from_string='EUR', to_string='USD') == 1.35
+
 
 def test_multiple_conversions():
     rates = [('USD', 'EUR', .74),
              ('USD', 'JPY', 145.949),
              ('USD', 'CAD', 1.21)]
-    assert convert(rates, value = 500,
-                   from_string = 'USD', to_string = 'EUR') == 370
-    assert convert(rates, value = 500,
-                   from_string = 'EUR', to_string = 'USD') == 675.68
-    assert convert(rates, value = 500,
-                   from_string = 'USD', to_string = 'JPY') == 72974.5
-    assert convert(rates, value = 500,
-                   from_string = 'JPY', to_string = 'USD') == 3.43
-    assert convert(rates, value = 500,
-                   from_string = 'USD', to_string = 'CAD') == 605
+    assert convert(rates, value=500,
+                   from_string='USD', to_string='EUR') == 370
+    assert convert(rates, value=500,
+                   from_string='EUR', to_string='USD') == 675.68
+    assert convert(rates, value=500,
+                   from_string='USD', to_string='JPY') == 72974.5
+    assert convert(rates, value=500,
+                   from_string='JPY', to_string='USD') == 3.43
+    assert convert(rates, value=500,
+                   from_string='USD', to_string='CAD') == 605
+
+
+def test_exception_check():
+    rates = [('USD', 'EUR', .74),
+             ('USD', 'JPY', 145.949),
+             ('USD', 'CAD', 1.21)]
+    assert exception_check(rates, value=500,
+                           from_string='USD', to_string='EUR') == 370
+    assert exception_check(rates, value=500,
+                           from_string='USD', to_string='FART') == (
+        "Error message")
+    assert exception_check(rates, value=500,
+                           from_string='ABC', to_string='DEF') == (
+        "Error message")
